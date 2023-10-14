1b95cbda7968fbeda3e34aad8482248fdd40d5e1
Muhammad hassan <hassanhanjra900@gmail.com>
2023-10-14 00:46:21 -0400

    update: popover time

--- js/days.js
+++ js/days.js
@@ -1,12 +1,14 @@
 
 function daysLeft(){
-    alert("Hekii");
     const date1 = new Date();
     const date2 = new Date('11/23/2023');
     const diffTime = Math.abs(date2 - date1);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 
     document.getElementById("days-left").innerHTML = diffDays;
+    setTimeout(function() {
+        document.getElementById("getting-access").click();
+      }, 5000);
 }
 
 daysLeft();
