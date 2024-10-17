16663e5518c55dd82236e8f2406acf4cf3ccac67
Muhammad hassan <hassanhanjra900@gmail.com>
2024-10-16 23:33:02 -0400

    update

--- js/days.js
+++ js/days.js
@@ -1,7 +1,7 @@
 
 function daysLeft(){
     const date1 = new Date();
-    const date2 = new Date('6/01/2024');
+    const date2 = new Date('01/01/2025');
     const diffTime = Math.abs(date2 - date1);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 
