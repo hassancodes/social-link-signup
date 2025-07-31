6721d168726595cfeb4e0346167b7445e7ec5c1b
Muhammad hassan <hassanhanjra900@gmail.com>
2025-07-31 19:02:43 -0400

    update

--- js/days.js
+++ js/days.js
@@ -1,14 +1,13 @@
-
-function daysLeft(){
+function daysLeft() {
     const date1 = new Date();
-    const date2 = new Date('01/01/2026');
+    const date2 = new Date('2026-01-01');
     const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
     document.getElementById("days-left").innerHTML = diffDays;
-    setTimeout(function() {
+    setTimeout(() => {
         document.getElementById("getting-access").click();
-      }, 5000);
+    }, 5000);
 }
 
 daysLeft();
